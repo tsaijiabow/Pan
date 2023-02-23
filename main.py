02b51d81e7b00b66c3fa33304a553e09cf280227
tsaijiabow <74822892+tsaijiabow@users.noreply.github.com>
2023-02-24 07:53:25 +0800

    ver 1.07

--- main.py
+++ main.py
@@ -10,49 +10,12 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='', intents=intents)
-with open('setting.json', 'r', encoding='utf-8') as tk:
-  token = json.load(tk)
+bot = commands.Bot(command_prefix='=“', intents=intents)
 
-rmsg = [
-     '說了就是有'
-    ,'絕對是的'
-    ,'沒可能'
-    ,'你媽媽表現此次'
-    ,'我現在不想理你'
-    ,'操你媽'
-    ,'一定可以的 對吧'
-    ,'？？？'
-    ,'割'
-    ,'欸'
-    ,'對'
-    ,'好'
-    ,'100%表示認同'
-    ,'完全正確'
-    ,'Yes'
-    ,'Fuck you'
-    ,'我表示非常認同'
-    ,'你在想什麼'
-    ,'我不知道'
-    ,'無法理解'
-    ,'衝三小'
-    ,'當然'
-    ,'是喔'
-    ,'沒錯'
-    ,'救命'
-    ,'笑死'
-    ,'確實'
-    ,'答錯了'
-    ,'嗯嗯嗯嗯是是是你說的都對'
-    ,'有時間在這裡講幹話還不如去打手槍'] #30個隨機回答
-luck = [
-       '大吉 去買個樂透看看'
-      ,'中吉 可能會有意料外的小驚喜'
-      ,'小吉 今天非常適合刷分 ~~不是曉極~~'
-      ,'普通 就跟平常一樣 普通的一天'
-      ,'小凶 感覺會有點不太順利'
-      ,'大凶 唉沒救了 準備等死吧'
-]
+with open('setting.json', 'r', encoding='utf-8') as st:
+  setjson = json.load(st)
+with open('reply.json', 'r', encoding='utf-8') as rp:
+  rmsg = json.load(rp)
 
 @bot.event
 async def on_ready():
@@ -82,12 +45,12 @@ async def on_message(ctx):
         await ctx.reply(f'{round(bot.latency*1000)}(ms)')
 
       elif x1 == 'version': #查目前版本
-        ver = ('目前版本 1.05\n增加功能：查單曲R值\n新增回覆：恭喜'+user+'突破理論值 海放眾人！')
+        ver = ('目前版本 1.07\n本次改動：縮短部分程式碼')
         await ctx.reply(ver)
 
-      elif x1 == 'luck': #測運氣
-        rl = choice(luck) 
-        await ctx.reply(rl)
+      elif x1 == 'luck': #抽籤
+        rl = randint(50,55)
+        await ctx.reply(rmsg[str(rl)])
 
       elif x1 == 'random': #隨機數字
         if x == 4: #判斷是否為兩個選項
@@ -125,35 +88,44 @@ async def on_message(ctx):
                 await ctx.reply('哇！恭喜 '+user+' 突破理論值 海放眾人！')
               elif x3 <= 101 and x3 >= 100.5:
                 rank = 100.5*x4*22.4*0.01
+                rank = round(rank,5)
                 await ctx.reply('定數 '+str(x4)+' 的譜面打到 '+str(x3)+'% 的R值是 '+ str(rank))
               elif x3 < 100.5 and x3 >= 100:
                 rank = x3x4*21.6*0.01
+                rank = round(rank,5)
                 await ctx.reply('定數 '+str(x4)+' 的譜面打到 '+str(x3)+'% 的R值是 '+ str(rank))
               elif x3 < 100 and x3 >= 99.5: 
                 rank = x3x4*21.1*0.01
+                rank = round(rank,5)
                 await ctx.reply('定數 '+str(x4)+' 的譜面打到 '+str(x3)+'% 的R值是 '+ str(rank))
               elif x3 < 99.5 and x3 >= 99: 
                 rank = x3x4*20.8*0.01
+                rank = round(rank,5)
                 await ctx.reply('定數 '+str(x4)+' 的譜面打到 '+str(x3)+'% 的R值是 '+ str(rank))
               elif x3 < 99 and x3 >= 98:
                 rank = x3x4*20.3*0.01
+                rank = round(rank,5)
                 await ctx.reply('定數 '+str(x4)+' 的譜面打到 '+str(x3)+'% 的R值是 '+ str(rank))
               elif x3 < 98 and x3 >= 97:
                 rank = x3x4*20*0.01
+                rank = round(rank,5)
                 await ctx.reply('定數 '+str(x4)+' 的譜面打到 '+str(x3)+'% 的R值是 '+ str(rank))
               elif x3 < 97 and x3 >= 94:
                 rank = x3x4*16.8*0.01
+                rank = round(rank,5)
                 await ctx.reply('定數 '+str(x4)+' 的譜面打到 '+str(x3)+'% 的R值是 '+ str(rank))
               elif x3 < 94 and x3 >= 90:
                 rank = x3x4*15.2*0.01
+                rank = round(rank,5)
                 await ctx.reply('定數 '+str(x4)+' 的譜面打到 '+str(x3)+'% 的R值是 '+ str(rank))
               elif x3 < 80 and x3 >= 90:
                 rank = x3x4*13.6*0.01
+                rank = round(rank,5)
                 await ctx.reply('定數 '+str(x4)+' 的譜面打到 '+str(x3)+'% 的R值是 '+ str(rank))
               else:
                 await ctx.reply('分數太低了！廢物！')
             else:
-              await ctx.reply('無法計算 你行你上')
+              await ctx.reply('給我分數跟定數')
           else:
             await ctx.reply('你要算什麼')
         else:
@@ -161,8 +133,8 @@ async def on_message(ctx):
 
           
       else:
-        rm = choice(rmsg) #隨機抽一條回覆
-        await ctx.reply(rm)
+        rm = randint(1,30) #隨機抽一條回覆
+        await ctx.reply(rmsg[str(rm)])
 
 keep_alive.keep_alive()
-bot.run(token["token"])
+bot.run(setjson["token"])
